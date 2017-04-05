b9d1f44bcf166cee28aaf7dbd5adc56e0b4ed0ab
demirhank <demirhand@meeapps.com.tr>
2017-04-05 21:39:09 +0300

    initial commit

---
+++ app/src/main/java/com/theappbusiness/marvel/ApplicationController.java
@@ -0,0 +1,26 @@
+package com.theappbusiness.marvel;
+
+
+import android.app.Application;
+
+/**
+ * Created by demir on 29.08.2016.
+ */
+public class ApplicationController extends Application {
+    private static ApplicationController mInstance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mInstance = this;
+
+        NetworkManager.initialize(getApplicationContext());
+    }
+
+    public static ApplicationController getInstance()
+    {
+        return mInstance;
+    }
+
+
+}
